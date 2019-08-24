0b0e278e5cc9f3b883024ffa995883218fb784ff
sonamabrol <49254707+sonamabrol@users.noreply.github.com>
2019-08-24 18:26:14 -0400

    added new line and made changes to the existing texts.

--- src/com/syed/conditions/ConditionsLearning.java
+++ src/com/syed/conditions/ConditionsLearning.java
@@ -5,13 +5,15 @@ public class ConditionsLearning {
 	public static void main(String[] args) {
 		
 	String myValue="Arlington";
-	if(myValue.equals("Ashburn")){
+	if(myValue.equals("Aburn")){
 		System.out.println("Iam in ashburn");		
 	}else if(myValue.equals("alexendria")){
-		System.out.println("I am in alexendria");
+		System.out.println("I am in alexendr");
 	}else{
-		System.out.println("not is ashburn and in alexendria");
+		System.out.println("not is ashburn and in alexendra");
 	}
+
+		System.out.println("adding to git, to check the connection");
 
 	}
 
